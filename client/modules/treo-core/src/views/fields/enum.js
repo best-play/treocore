caa5c38d3d9453b2c1ab09b08bdcc6aae4f8b355
Dima S <dimka@3g.ua>
2019-09-16 16:30:50 +0300

    init workflow

---
+++ client/modules/treo-core/src/views/fields/enum.js
@@ -0,0 +1,58 @@
+/*
+ * This file is part of EspoCRM and/or TreoCore.
+ *
+ * EspoCRM - Open Source CRM application.
+ * Copyright (C) 2014-2019 Yuri Kuznetsov, Taras Machyshyn, Oleksiy Avramenko
+ * Website: http://www.espocrm.com
+ *
+ * TreoCore is EspoCRM-based Open Source application.
+ * Copyright (C) 2017-2019 TreoLabs GmbH
+ * Website: https://treolabs.com
+ *
+ * TreoCore as well as EspoCRM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TreoCore as well as EspoCRM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EspoCRM. If not, see http://www.gnu.org/licenses/.
+ *
+ * The interactive user interfaces in modified source and object code versions
+ * of this program must display Appropriate Legal Notices, as required under
+ * Section 5 of the GNU General Public License version 3.
+ *
+ * In accordance with Section 7(b) of the GNU General Public License version 3,
+ * these Appropriate Legal Notices must retain the display of the "EspoCRM" word
+ * and "TreoCore" word.
+ */
+
+Espo.define('treo-core:views/fields/enum', 'class-replace!treo-core:views/fields/enum', function (Dep) {
+
+    return Dep.extend({
+        setupOptions: function() {
+            this.listenToOnce(this.model, 'before:save', () => {
+                let entityType = this.model.getEntityType();
+                let fieldType = this.name;
+                let workflow = this.getMetadata().get(['workflow', entityType, fieldType]);
+
+                if (!workflow)
+                    return false;
+
+                let prevAttrValue = this.model.lastXhr.responseJSON[fieldType],
+                    currentValueAttr = this.model.get(fieldType),
+                    workflowArray = this.model.isNew() ? workflow['initStates'] : workflow['transitions'][currentValueAttr];
+
+                if ($.inArray(prevAttrValue, workflowArray) === -1) {
+                    let msg = this.translate('transitionIsForbidden', 'messages').replace('{field}', this.getLabelText());
+                    this.showValidationMessage(msg);
+                    return true;
+                }
+            });
+        },
+    });
+});
